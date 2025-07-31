b02e802a756b47f69c74617397357ec2e17a685c
coding-cat0-0 <khawajab302@email.com>
2025-07-31 16:43:25 +0500

    Update: fixed task upload and WebSocket notifications

--- websockets_router/login_websocket.py
+++ websockets_router/login_websocket.py
@@ -8,13 +8,13 @@ active_connections : dict[str, WebSocket]={}
 
 @router.websocket('/ws/login/notification')
 async def websocket_endpoint(websocket : WebSocket, db:Session = Depends(get_db)):
+    current_ws = await get_current_ws(websocket, db)
+    if not current_ws:
+        print("User validation failed. Closing socket.")
+        return
     await websocket.accept() # opens the connection
     print("connection open")
 
-    current_ws = await get_current_ws(websocket, db)
-    if not current_ws:
-        #print("User validation failed. Closing socket.")
-        return
     email =   current_ws.email 
 
 #    (
@@ -24,6 +24,7 @@ async def websocket_endpoint(websocket : WebSocket, db:Session = Depends(get_db)
     
     active_connections[email] = websocket
     print("WebSocket stored email:", email)
+    print(active_connections)
     try: 
         while True:
             data = await websocket.receive_text() # keeps it alive not significant what you put here
